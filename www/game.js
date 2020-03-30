915d6b2c45851cc091458296ae305ab4c374d191
Tom O'Donnell <dev.todonnell@gmail.com>
2020-03-30 13:01:41 -0400

    Fixed Distance Text Location
    A new HUD scene was created to display stuff to the user without
    being affected by the gameboard's camera. It independently listens
    for all gameobject drag events to update the distance text.
    
    While a step toward refactoring the code from this doo doo, I just
    threw the existing scene stuff into the new Gameboard class.

--- www/game.js
+++ www/game.js
@@ -1,5 +1,5 @@
 // Set some config type stuff
-let websocket, cursors, wasd, distanceText;
+let websocket, cursors, wasd;
 
 const canvasSize = 900;
 const gridSquareSize = 64;
@@ -21,7 +21,7 @@ const colors = {
     "yellow": 0xffff00,
     "lime": 0x00ff00,
     "majenta": 0xff00ff,
-    // "white": 0xffffff,
+    "white": 0xffffff,
     "silver": 0xc0c0c0,
     "gray": 0x808080,
     "black": 0x000000,
@@ -50,141 +50,171 @@ tokenSubmit.onclick = function (event) {
     generateToken(scene, null, null, shape, color, depth);
 }
 
+var HudScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    initialize:
+    function HudScene() {
+        Phaser.Scene.call(this, { key: 'hud' });
+    },
+
+    create: function () {
+        // Create text object for distance measure
+        const distanceText = this.add.text(10, 10).setFontSize(24).setColor('#ff00ff').setText('Distance: 0');
+
+        // Hook into game objects' drag events to update distance
+        const gameboard = this.scene.get('gameboard');
+        gameboard.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+            // Update distance measure from pointer to original token location
+            let deltaX = pointer.worldX - gameObject.input.dragStartX;
+            let deltaY = pointer.worldY - gameObject.input.dragStartY;
+            let distance = Math.sqrt(deltaX**2 + deltaY**2);
+
+            // Show distance in grid squares rounded to a single decimal place
+            let prettyDistance = Math.round(distance/gridSquareSize * 10) / 10;
+            distanceText.setText('Distance: ' + prettyDistance);
+        });
+    },
+});
+
+var GameboardScene = new Phaser.Class({
+    Extends: Phaser.Scene,
+
+    initialize:
+    function GameboardScene() {
+        Phaser.Scene.call(this, { key: 'gameboard' });
+    },
+
+    preload: function () {
+        // this.load.image('background', '/static/plasma.png');
+        this.load.image('virus', '/static/virus.png');
+    },
+
+    create: function () {
+        // Load the websocket stuff here so images are loaded
+        websocket = new WebSocket("ws://192.168.1.19:6789/");
+
+        // Setup some input
+        cursors = this.input.keyboard.createCursorKeys();
+        wasd = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.W,
+            down: Phaser.Input.Keyboard.KeyCodes.S,
+            left: Phaser.Input.Keyboard.KeyCodes.A,
+            right: Phaser.Input.Keyboard.KeyCodes.D,
+            ctrl: Phaser.Input.Keyboard.KeyCodes.CTRL,
+        });
+
+        this.cameras.main.setBounds(0, 0, fieldSize, fieldSize);
+        this.cameras.main.zoom = 0.5;
+
+        minus.onclick = function (event) {
+            websocket.send(JSON.stringify({action: 'minus'}));
+        }
+
+        plus.onclick = function (event) {
+            websocket.send(JSON.stringify({action: 'plus'}));
+        }
+
+        websocket.onmessage = function (event) {
+            data = JSON.parse(event.data);
+            switch (data.type) {
+                case 'state':
+                    value.textContent = data.value;
+
+                    const scene = game.scene.scenes[0];
+                    if (!scene) {
+                        break;
+                    }
+
+                    let re = RegExp('^token\d*');
+                    let tokenNames = Object.keys(data).filter((k) => re.test(k));
+                    tokenNames.forEach((name) => {
+                        const token = scene.children.getByName(name);
+                        if (token) {
+                            token.x = data[name].x;
+                            token.y = data[name].y;
+                        }
+                        else {
+                            generateToken(scene, data[name].x, data[name].y, data[name].shape, data[name].color, data[name].depth, name)
+                        }
+                    });
+
+                    break;
+                case 'users':
+                    users.textContent = (data.count.toString() + " user" + (data.count == 1 ? "" : "s"));
+                    break;
+                default:
+                    console.error("unsupported event", data);
+            }
+        };
+
+        // Create the background
+        // TODO:  make tileable background with gimp and create texture for bg
+        // note:  canvas texture could also generate gameboards, for say checkers
+        // const background = this.add.image(containerSize/2, containerSize/2, 'background');
+
+        // Create the gameboard
+        const grid = this.add.grid(fieldSize/2, fieldSize/2, gridSize, gridSize, gridSquareSize, gridSquareSize, gridColor, gridAlpha, gridLineColor, gridLineAlpha);
+        grid.name = 'gameboard';
+        grid.depth = 1;
+
+        // Request token information
+        let wsState = websocket.readyState;
+        if (wsState == websocket.OPEN) {
+            websocket.send(JSON.stringify({action: 'ping'}))
+        }
+        else {
+            websocket.onopen = () => {websocket.send(JSON.stringify({action: 'ping'}))};
+        }
+
+        this.scene.launch('hud');
+    },
+
+    update: function () {
+        let cam = this.cameras.main;
+        let speed = cursors.shift.isDown ? 16 : 8;
+
+        if (Phaser.Input.Keyboard.JustDown(cursors.space))
+        {
+            let newZoom = cam.zoom == 1 ? 0.5 : 1;
+            cam.zoomTo(newZoom);
+        }
+
+        if (cursors.left.isDown || wasd.left.isDown)
+        {
+            cam.scrollX -= speed;
+        }
+        else if (cursors.right.isDown || wasd.right.isDown)
+        {
+            cam.scrollX += speed;
+        }
+
+        if (cursors.up.isDown || wasd.up.isDown)
+        {
+            cam.scrollY -= speed;
+        }
+        else if (cursors.down.isDown || wasd.down.isDown)
+        {
+            cam.scrollY += speed;
+        }
+    },
+});
+
 let config = {
     parent: 'phaser-container',
     type: Phaser.AUTO,
     width: canvasSize,
     height: canvasSize,
     backgroundColor: 0xf5f5dc,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update,
-    },
+    scene: [
+        GameboardScene,
+        HudScene,
+    ],
 };
 
 const game = new Phaser.Game(config);
 
 let tokenCounter = 0;   // TODO:  make better
-
-function preload ()
-{
-    // this.load.image('background', '/static/plasma.png');
-    this.load.image('virus', '/static/virus.png');
-}
-
-function create ()
-{
-    // Load the websocket stuff here so images are loaded
-    websocket = new WebSocket("ws://192.168.1.19:6789/");
-
-    // Setup some input
-    cursors = this.input.keyboard.createCursorKeys();
-    wasd = this.input.keyboard.addKeys({
-        up: Phaser.Input.Keyboard.KeyCodes.W,
-        down: Phaser.Input.Keyboard.KeyCodes.S,
-        left: Phaser.Input.Keyboard.KeyCodes.A,
-        right: Phaser.Input.Keyboard.KeyCodes.D,
-        ctrl: Phaser.Input.Keyboard.KeyCodes.CTRL,
-    });
-
-    this.cameras.main.setBounds(0, 0, fieldSize, fieldSize);
-    this.cameras.main.zoom = 0.5;
-
-    distanceText = this.add.text(10, 10).setScrollFactor(0).setFontSize(64).setColor('#ff00ff').setText('Distance: 0');
-    distanceText.depth = 3;
-
-
-    minus.onclick = function (event) {
-        websocket.send(JSON.stringify({action: 'minus'}));
-    }
-
-    plus.onclick = function (event) {
-        websocket.send(JSON.stringify({action: 'plus'}));
-    }
-
-    websocket.onmessage = function (event) {
-        data = JSON.parse(event.data);
-        switch (data.type) {
-            case 'state':
-                value.textContent = data.value;
-
-                const scene = game.scene.scenes[0];
-                if (!scene) {
-                    break;
-                }
-
-                let re = RegExp('^token\d*');
-                let tokenNames = Object.keys(data).filter((k) => re.test(k));
-                tokenNames.forEach((name) => {
-                    const token = scene.children.getByName(name);
-                    if (token) {
-                        token.x = data[name].x;
-                        token.y = data[name].y;
-                    }
-                    else {
-                        generateToken(scene, data[name].x, data[name].y, data[name].shape, data[name].color, data[name].depth, name)
-                    }
-                });
-
-                break;
-            case 'users':
-                users.textContent = (data.count.toString() + " user" + (data.count == 1 ? "" : "s"));
-                break;
-            default:
-                console.error("unsupported event", data);
-        }
-    };
-
-    // Create the background
-    // TODO:  make tileable background with gimp and create texture for bg
-    // note:  canvas texture could also generate gameboards, for say checkers
-    // const background = this.add.image(containerSize/2, containerSize/2, 'background');
-
-    // Create the gameboard
-    const grid = this.add.grid(fieldSize/2, fieldSize/2, gridSize, gridSize, gridSquareSize, gridSquareSize, gridColor, gridAlpha, gridLineColor, gridLineAlpha);
-    grid.name = 'gameboard';
-    grid.depth = 1;
-
-    // Request token information
-    let wsState = websocket.readyState;
-    if (wsState == websocket.OPEN) {
-        websocket.send(JSON.stringify({action: 'ping'}))
-    }
-    else {
-        websocket.onopen = () => {websocket.send(JSON.stringify({action: 'ping'}))};
-    }
-}
-
-function update ()
-{
-    let cam = this.cameras.main;
-    let speed = cursors.shift.isDown ? 16 : 8;
-
-    if (Phaser.Input.Keyboard.JustDown(cursors.space))
-    {
-        let newZoom = cam.zoom == 1 ? 0.5 : 1;
-        cam.zoomTo(newZoom);
-    }
-
-    if (cursors.left.isDown || wasd.left.isDown)
-    {
-        cam.scrollX -= speed;
-    }
-    else if (cursors.right.isDown || wasd.right.isDown)
-    {
-        cam.scrollX += speed;
-    }
-
-    if (cursors.up.isDown || wasd.up.isDown)
-    {
-        cam.scrollY -= speed;
-    }
-    else if (cursors.down.isDown || wasd.down.isDown)
-    {
-        cam.scrollY += speed;
-    }
-}
 
 function generateToken(scene, x, y, shape, color, depth=2, name=null)
 {
@@ -243,15 +273,6 @@ function generateToken(scene, x, y, shape, color, depth=2, name=null)
         // it plays well with different zooms and dragging objects while scrolling.
         let newX = wasd.ctrl.isDown ? token.input.dragStartX : pointer.worldX;
         let newY = wasd.ctrl.isDown ? token.input.dragStartY : pointer.worldY;
-
-        // Update distance measure from pointer to original token location
-        let deltaX = pointer.worldX - token.input.dragStartX;
-        let deltaY = pointer.worldY - token.input.dragStartY;
-        let distance = Math.sqrt(deltaX**2 + deltaY**2);
-
-        // Show distance in grid squares rounded to a single decimal place
-        let prettyDistance = Math.round(distance/gridSquareSize * 10) / 10;
-        distanceText.setText('Distance: ' + prettyDistance);
 
         if (token.x != newX || token.y != newY) {
 
